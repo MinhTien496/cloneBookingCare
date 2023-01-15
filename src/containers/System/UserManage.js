9aa0904756c18848a2c5c0e58d92dafdc651c7b8
Minh Tien <minhtien4962001@gmail.com>
2023-01-15 22:05:39 +0700

    15.1.2023

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getDataUser } from '../../services/userService'
+
 class UserManage extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrDataUser: []
+        }
     }
 
-    componentDidMount() {
+    async componentDidMount() {
+        let response = await getDataUser('ALL');
+        console.log('arrdata0', response)
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrDataUser: response.userData
+            }, () => {
+                console.log('arrdata1', response.userData)
+            })
+            console.log('arrdata2', this.state.arrDataUser)
 
+        }
     }
 
 
     render() {
+        let arrDataUser = this.state.arrDataUser
         return (
-            <div className="text-center">Manage users</div>
+            <div className="user-container">
+                <div className="title text-center">Manage users</div>
+                <div className="table-data mt-3 mx-2">
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Active</th>
+                        </tr>
+                        {   arrDataUser && arrDataUser.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <td>Edit me</td>
+                                </tr>
+                            )
+                        })
+                        }
+                    </table>
+                </div>
+            </div>
         );
     }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import { stringify } from 'react-auth-wrapper/helpers';
 import axios from '../axios'
-
+//Req to backend with email and password from client
 const handleLoginApi = (userEmailInput, userPasswordInput) => {
     return axios.post('/api/login', {
         email: userEmailInput,
@@ -8,4 +8,8 @@ const handleLoginApi = (userEmailInput, userPasswordInput) => {
     });
 }
 
-export { handleLoginApi }
+const getDataUser = (inputId) => {
+    return axios.get(`/api/get-all-users?id=${inputId}`);
+}
+
+export { handleLoginApi, getDataUser }
